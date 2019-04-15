0942f399a2d7e3850954895bd2ad90616b684a6e
yuzuzhi <yuzuzhi@sina.com>
2019-04-15 19:32:44 +0800

    添加主界面菜单

--- Editor/NodeEditorWindow.cs
+++ Editor/NodeEditorWindow.cs
@@ -10,21 +10,70 @@ namespace cfeditor
         [MenuItem("Window/ssss")]
         static void ddd()
         {
-            var w = GetWindow<NodeEditorWindow>();
-            w.Show(true);
-            w.nodeWnd = new NodeWindow(w);
-            w.nodeWnd.target = AssetDatabase.LoadAssetAtPath<ScriptableObject>("Assets/Editor/NodeEditor/sss.asset");
+            Self = GetWindow<NodeEditorWindow>();
+            Self.Show(true);
+            var data = AssetDatabase.LoadAssetAtPath<ScriptableObject>("Assets/Editor/NodeEditor/sss.asset");
+            if (data != null)
+            {
+                Self.nodeWnd = new NodeWindow(Self);
+                Self.nodeWnd.target = data;
+            }
         }
 
         void OnGUI()
         {
             BeginWindows();
+            if (nodeWnd != null)
+                nodeWnd.OnGUI();
 
-            nodeWnd.OnGUI();
+            ProcessEvents(Event.current);
             EndWindows();
         }
 
 
+
+
+
+        private void ProcessEvents(Event e)
+        {
+
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    if (e.button == 0)
+                    {
+                        //ClearConnectionSelection();
+                    }
+
+                    if (e.button == 1)
+                    {
+                        ProcessContextMenu(e.mousePosition);
+                    }
+                    break;
+
+                case EventType.MouseDrag:
+                    if (e.button == 0)
+                    {
+                        //OnDrag(e.delta);
+                    }
+                    break;
+            }
+        }
+        private void ProcessContextMenu(Vector2 mousePosition)
+        {
+            GenericMenu genericMenu = new GenericMenu();
+            genericMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
+            genericMenu.ShowAsContext();
+        }
+        private void OnClickAddNode(Vector2 mousePosition)
+        {
+            if (nodeWnd == null)
+                nodeWnd = new NodeWindow(this);
+            Self.nodeWnd.target = CreateInstance<cfeditor.StudentConfObject>();
+            AssetDatabase.CreateAsset(Self.nodeWnd.target, "Assets/Editor/NodeEditor/sss.asset");
+        }
+
         private NodeWindow nodeWnd;
+        private static NodeEditorWindow Self;
     }
 }
--- Editor/StudentConfObject.cs
+++ Editor/StudentConfObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace cfeditor
 {
-
+    
     public class StudentConfObject : ConfObject<cfeditor.Student>
     {
     }
