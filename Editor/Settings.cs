e96f16f7771ff61ca438c50a5c1f9608eee74f7f
yuzuzhi <yuzuzhi@sina.com>
2019-04-17 17:54:00 +0800

    Settings

---
+++ Editor/Settings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace cfeditor
+{
+    [CreateAssetMenu]
+    public class Settings : ScriptableObject
+    {
+        public string confpath;
+
+
+
+        private static Settings m_Instance;
+        public static Settings Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    var finded = AssetDatabase.FindAssets("t:Settings");
+                    if (finded.Length > 0)
+                    {
+                        var path = AssetDatabase.GUIDToAssetPath(finded[0]);
+                        m_Instance = AssetDatabase.LoadAssetAtPath<Settings>(path);
+                    }
+                }
+                return m_Instance;
+            }
+        }
+
+        public static string rootPath
+        {
+            get
+            {
+                if (m_Instance != null)
+                {
+                    return AssetDatabase.GetAssetPath(m_Instance);
+                }
+                return "Assets/Editor/";
+            }
+        }
+
+        public static string resPath
+        {
+            get { return rootPath + "res/"; }
+        }
+
+        
+    }
+}
